1312a01f120323fd38cc4cb360694ef9d6d8c0e9
KaraokeGod <austinly1997@gmail.com>
2015-11-12 14:13:48 -0500

    Begin adding bluetooth code
    Added MAC address of the Bluetooth modem

--- app/src/main/java/com/hieu/riseandshine/MainActivity.java
+++ app/src/main/java/com/hieu/riseandshine/MainActivity.java
@@ -1,15 +1,25 @@
 package com.hieu.riseandshine;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
+    private static String address = "98:D3:31:90:4D:97";
+
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
